e84a4ac76c4572e4e95bc4f6f3478861ed11492c
amirbigg <amirbig44@gmail.com>
2022-11-24 12:40:06 +0330

    changed

--- routers/users.py
+++ routers/users.py
@@ -8,8 +8,8 @@ from dependencies import get_db
 router = APIRouter()
 
 @router.post('/users/', response_model=schemas.User)
-async def create_user(user:schemas.UserCreate, db: Session = Depends(get_db)):
-	db_user = await db.query(models.User).filter(models.User.email==user.email).first()
+def create_user(user:schemas.UserCreate, db: Session = Depends(get_db)):
+	db_user = db.query(models.User).filter(models.User.email==user.email).first()
 	if db_user:
 		raise HTTPException(status_code=400, detail='Email already exists')
 	user = models.User(email=user.email, username=user.username, password=user.password)
@@ -20,8 +20,8 @@ async def create_user(user:schemas.UserCreate, db: Session = Depends(get_db)):
 
 
 @router.get('/users/{user_id}', response_model=schemas.User)
-async def read_user(user_id:int, db: Session = Depends(get_db)):
-	db_user = await db.query(models.User).filter(models.User.id==user_id).first()
+def read_user(user_id:int, db: Session = Depends(get_db)):
+	db_user = db.query(models.User).filter(models.User.id==user_id).first()
 	if db_user is None:
 		raise HTTPException(status_code=404, detail='User not found')
 	return db_user
