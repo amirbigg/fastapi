689672aac310bd7c94c262f6fae36eb1bcb9779b
amirbigg <amirbig44@gmail.com>
2022-11-26 16:17:50 +0330

    add ep11

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from routers import users
 import time
+import datetime
 
 
 app = FastAPI()
@@ -14,3 +15,15 @@ async def add_process_time_header(request, call_next):
 	process_time = time.time() - start_time
 	response.headers['X-Process-Time'] = str(process_time)
 	return response
+
+
+@app.on_event('startup')
+def startup_event():
+	with open('server_time_log.log', 'a') as log:
+		log.write(f'Application started at: {datetime.datetime.now()} \n')
+
+
+@app.on_event('shutdown')
+def shutdown_event():
+	with open('server_time_log.log', 'a') as log:
+		log.write(f'Application shut down at: {datetime.datetime.now()} \n')
